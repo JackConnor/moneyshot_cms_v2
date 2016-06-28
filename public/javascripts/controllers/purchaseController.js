05c4d089fa245e8c5647f7d94fe402f6c8700d67
Jack Connor <jack.connor83@gmail.com>
2016-06-28 13:52:40 -0700

    now adding transactions to app

--- public/javascripts/controllers/purchaseController.js
+++ public/javascripts/controllers/purchaseController.js
@@ -6,23 +6,50 @@ angular.module('purchaseController', [])
 
   function purchaseCtrl($http){
     var self = this;
+    self.purchaseModalBool = false;
+
+
 
     function sendPhotoPrice(){
-      var price = $('#purchasePhotoPrice').val();
+      // var price = $('#purchasePhotoPrice').val();
       var photoId = $('#purchasePhotoId').val();
-      var photoPublication = $('#purchasePhotoPublication').val();
-      console.log(price + photoId);
+      var price = $('#purchasePhotoPrice').val();
+      self.userPrice = price;
+      // var photoPublication = $('#purchasePhotoPublication').val();
+      // console.log(price + photoId);
       $http({
-        method: "POST"
-        ,url: 'http://192.168.0.122:5555/api/soldPhoto'
-        ,data: {photoId: photoId, photoPrice: price}
+        method: "GET"
+        ,url: "http://192.168.0.9:5555/api/photo/"+photoId
       })
       .then(function(data){
         console.log(data);
-        ///////this needs to be an email callback confirming that they received their email
+        self.purchaseModalBool = true;
+        self.photoToSell = data.data;
       })
     }
     self.sendPhotoPrice = sendPhotoPrice;
+
+    function sendEmail(){
+      var price = $('#purchasePhotoPrice').val();
+      var photoId = $('#purchasePhotoId').val();
+      var photoPublication = $('#purchasePhotoPublication').val();
+      $http({
+        method: "POST"
+        ,url: 'http://192.168.0.9:5555/api/soldPhoto'
+        ,data: {photoId: photoId, photoPrice: price, purchaser: photoPublication}
+      })
+      .then(function(data){
+        console.log(data);
+        if(data.data.message = "Success"){
+          alert('Your Photo has been sent!')
+          // window.location.reload();
+        }
+        else {
+          alert('There was a problem, please try again');
+        }
+      })
+    }
+    self.sendEmail = sendEmail;
 
   //////end controller
   }
